cd795e9f63447f6a779181b4707a8ba3e0b47dcd
harsha-uppalapati <harshau91@gmail.com>
2024-06-15 21:22:43 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./App.css"
+const App = () => {
+  return (
+    <>
+      <div className="card">
+        <img src="https://via.placeholder.com/400x200" alt="Card image" />
+        <div className="card-content">
+          <h2 className="card-title">Card Title</h2>
+          <p className="card-description">
+            This is a description of the card. It contains some additional
+            information about the content of the card.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
