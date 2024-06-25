f9849a0ef6e289f8c6f9938d3eafbc8b47ff2ec1
harsha-uppalapati <harshau91@gmail.com>
2024-06-26 02:58:34 +0530

    latesr

--- src/Components/Celebration.js
+++ src/Components/Celebration.js
@@ -1,80 +1,80 @@
 // Celebration.js
 
-import React, { useState } from "react";
+// import React, { useState } from "react";
 // import { useSpring, animated } from "react-spring";
 import "./Celebration.css"; // Import the CSS file for styling
 import Nav from "./Nav";
-const COLORS = [
-  "#FF6633",
-  "#FFB399",
-  "#FF33FF",
-  "#FFFF99",
-  "#00B3E6",
-  "#E6B333",
-  "#3366E6",
-  "#999966",
-  "#99FF99",
-  "#B34D4D",
-  "#80B300",
-  "#809900",
-  "#E6B3B3",
-  "#6680B3",
-  "#66991A",
-  "#FF99E6",
-  "#CCFF1A",
-  "#FF1A66",
-  "#E6331A",
-  "#33FFCC",
-  "#66994D",
-  "#B366CC",
-  "#4D8000",
-  "#B33300",
-  "#CC80CC",
-  "#66664D",
-  "#991AFF",
-  "#E666FF",
-  "#4DB3FF",
-  "#1AB399",
-  "#E666B3",
-  "#33991A",
-  "#CC9999",
-  "#B3B31A",
-  "#00E680",
-  "#4D8066",
-  "#809980",
-  "#E6FF80",
-  "#1AFF33",
-  "#999933",
-  "#FF3380",
-  "#CCCC00",
-  "#66E64D",
-  "#4D80CC",
-  "#9900B3",
-  "#E64D66",
-  "#4DB380",
-  "#FF4D4D",
-  "#99E6E6",
-  "#6666FF",
-];
+// const COLORS = [
+//   "#FF6633",
+//   "#FFB399",
+//   "#FF33FF",
+//   "#FFFF99",
+//   "#00B3E6",
+//   "#E6B333",
+//   "#3366E6",
+//   "#999966",
+//   "#99FF99",
+//   "#B34D4D",
+//   "#80B300",
+//   "#809900",
+//   "#E6B3B3",
+//   "#6680B3",
+//   "#66991A",
+//   "#FF99E6",
+//   "#CCFF1A",
+//   "#FF1A66",
+//   "#E6331A",
+//   "#33FFCC",
+//   "#66994D",
+//   "#B366CC",
+//   "#4D8000",
+//   "#B33300",
+//   "#CC80CC",
+//   "#66664D",
+//   "#991AFF",
+//   "#E666FF",
+//   "#4DB3FF",
+//   "#1AB399",
+//   "#E666B3",
+//   "#33991A",
+//   "#CC9999",
+//   "#B3B31A",
+//   "#00E680",
+//   "#4D8066",
+//   "#809980",
+//   "#E6FF80",
+//   "#1AFF33",
+//   "#999933",
+//   "#FF3380",
+//   "#CCCC00",
+//   "#66E64D",
+//   "#4D80CC",
+//   "#9900B3",
+//   "#E64D66",
+//   "#4DB380",
+//   "#FF4D4D",
+//   "#99E6E6",
+//   "#6666FF",
+// ];
 
 const Celebration = ({ isVisible }) => {
-  const [papers, setPapers] = useState([]);
+  // const [papers, setPapers] = useState([]);
 
-  const handleClick = () => {
-    const paperCount = 150; // Number of papers to fall
+  // const handleClick = () => {
+  //   const paperCount = 150; // Number of papers to fall
 
-    // Generate papers with animation properties
-    const newPapers = Array.from({ length: paperCount }, () => {
-      const key = `paper-${Date.now()}-${Math.random()}`;
-      const color = COLORS[Math.floor(Math.random() * COLORS.length)];
-      const delay = Math.random() * 1000;
+  //   // Generate papers with animation properties
+  //   const newPapers = Array.from({ length: paperCount }, () => {
+  //     const key = `paper-${Date.now()}-${Math.random()}`;
+  //     const color = COLORS[Math.floor(Math.random() * COLORS.length)];
+  //     const delay = Math.random() * 1000;
 
-      return <></>;
-    });
+  //     return <></>;
+  //   });
 
-    // Add new papers to the state
-    setPapers(prevPapers => [...prevPapers, ...newPapers]);
-  };
+  //   // Add new papers to the state
+  //   setPapers(prevPapers => [...prevPapers, ...newPapers]);
+  // };
 
   return (
     <div>
@@ -83,7 +83,7 @@ const Celebration = ({ isVisible }) => {
         ColourPop
       </button> */}
 
-      <div className="papers-container">{papers}</div>
+      {/* <div className="papers-container">{papers}</div> */}
     </div>
   );
 };
